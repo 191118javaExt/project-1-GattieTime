5ea1d21887a227d56ddeef64123723a0a4dda784
GattieTime <gattietime@gmail.com>
2020-01-13 10:12:22 -0500

    Added more tests

--- src/test/java/com/revature/services/ReinburseServiceTest.java
+++ src/test/java/com/revature/services/ReinburseServiceTest.java
@@ -2,6 +2,7 @@ package com.revature.services;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNotEquals;
 import static org.junit.Assert.assertTrue;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
@@ -34,6 +35,14 @@ public class ReinburseServiceTest {
 	}
 	
 	@Test
+	public void testReinbursResolveFalse() {
+		User u = new User();
+		Reinbursement rtest = new Reinbursement(1, 0, null, null, "test", "test", 2, 3, 4);
+		when(rDAOMock.updateReinburse(rtest)).thenReturn(false);
+		assertFalse(rsMock.reinbursResolve(u, rtest, 2));
+	}
+	
+	@Test
 	public void testNewReimburs() {
 		ReimbursementTemplate rt = new ReimbursementTemplate();
 		Reinbursement rtest = new Reinbursement();
@@ -57,10 +66,32 @@ public class ReinburseServiceTest {
 	}
 	
 	@Test
+	public void testGetUserReimbursNotEquals() {
+		
+		Reinbursement r1 = new Reinbursement();
+		Reinbursement r2 = new Reinbursement();
+		Reinbursement r3 = new Reinbursement();
+		List<Reinbursement> rl = new ArrayList<Reinbursement>();
+		rl.add(r1);
+		rl.add(r3);
+		rl.add(r2);
+		UserIdTemplate u = new UserIdTemplate(1);
+		when(rDAOMock.findByUserId(1)).thenReturn(rl);
+		assertNotEquals(4, rsMock.getUserReimburs(u).size());
+	}
+	
+	@Test
 	public void testNewReceipt() {
 		ReceiptTemplate rt = new ReceiptTemplate(1,"test");
 		when(rDAOMock.updateReceipt(rt)).thenReturn(true);
 		assertTrue(rsMock.newReceipt(rt));
+	}
+	
+	@Test
+	public void testNewReceiptFalse() {
+		ReceiptTemplate rt = new ReceiptTemplate(1,"test");
+		when(rDAOMock.updateReceipt(rt)).thenReturn(false);
+		assertFalse(rsMock.newReceipt(rt));
 	}
 	
 	@Test
@@ -80,12 +111,101 @@ public class ReinburseServiceTest {
 	}
 	
 	@Test
+	public void testGetReimbursAllNot() {
+
+		Reinbursement r1 = new Reinbursement();
+		Reinbursement r2 = new Reinbursement();
+		Reinbursement r3 = new Reinbursement();
+		List<Reinbursement> rl = new ArrayList<Reinbursement>();
+		rl.add(r1);
+		rl.add(r3);
+		rl.add(r2);
+		FindReimTemplate frt = new FindReimTemplate();
+		frt.setStatus(1);
+		when(rDAOMock.findAll()).thenReturn(rl);
+		assertNotEquals(4, rsMock.getReimburs(frt).size());
+	}
+	
+	@Test
+	public void testGetReimbursAllOrder() {
+
+		Reinbursement r1 = new Reinbursement();
+		Reinbursement r2 = new Reinbursement();
+		Reinbursement r3 = new Reinbursement();
+		List<Reinbursement> rl = new ArrayList<Reinbursement>();
+		rl.add(r1);
+		rl.add(r3);
+		rl.add(r2);
+		FindReimTemplate frt = new FindReimTemplate();
+		frt.setStatus(1);
+		when(rDAOMock.findAll()).thenReturn(rl);
+		assertNotEquals(r1, rsMock.getReimburs(frt).get(1));
+	}
+	
+	@Test
+	public void testGetReimbursSort() {
+
+		Reinbursement r1 = new Reinbursement();
+		Reinbursement r2 = new Reinbursement();
+		Reinbursement r3 = new Reinbursement();
+		List<Reinbursement> rl = new ArrayList<Reinbursement>();
+		rl.add(r1);
+		rl.add(r3);
+		rl.add(r2);
+		FindReimTemplate frt = new FindReimTemplate();
+		frt.setStatus(2);
+		when(rDAOMock.findByStatus(1)).thenReturn(rl);
+		assertEquals(3, rsMock.getReimburs(frt).size());
+	}
+	
+	@Test
+	public void testGetReimbursSortNot() {
+
+		Reinbursement r1 = new Reinbursement();
+		Reinbursement r2 = new Reinbursement();
+		Reinbursement r3 = new Reinbursement();
+		List<Reinbursement> rl = new ArrayList<Reinbursement>();
+		rl.add(r1);
+		rl.add(r3);
+		rl.add(r2);
+		FindReimTemplate frt = new FindReimTemplate();
+		frt.setStatus(2);
+		when(rDAOMock.findByStatus(1)).thenReturn(rl);
+		assertNotEquals(4, rsMock.getReimburs(frt).size());
+	}
+	
+	@Test
+	public void testGetReimbursSortOrder() {
+
+		Reinbursement r1 = new Reinbursement();
+		Reinbursement r2 = new Reinbursement();
+		Reinbursement r3 = new Reinbursement();
+		List<Reinbursement> rl = new ArrayList<Reinbursement>();
+		rl.add(r1);
+		rl.add(r3);
+		rl.add(r2);
+		FindReimTemplate frt = new FindReimTemplate();
+		frt.setStatus(2);
+		when(rDAOMock.findByStatus(1)).thenReturn(rl);
+		assertEquals(r1, rsMock.getReimburs(frt).get(0));
+	}
+	
+	@Test
 	public void testApproval() {
 		ApprovalTemplate at = new ApprovalTemplate();
 		Reinbursement r = new Reinbursement();
 		
 		when(rDAOMock.updateReinburse(r)).thenReturn(true);
 		assertTrue(rsMock.approval(at, r));
+	}
+	
+	@Test
+	public void testApprovalFalse() {
+		ApprovalTemplate at = new ApprovalTemplate();
+		Reinbursement r = new Reinbursement();
+		
+		when(rDAOMock.updateReinburse(r)).thenReturn(false);
+		assertFalse(rsMock.approval(at, r));
 	}
 
 }
